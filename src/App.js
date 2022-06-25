3be50072a8b3677aa85109a04f850c25f484f899
Aleksey <epsilonalexman@gmail.com>
2022-06-25 21:16:58 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import './styles/App.css'
+import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
+
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'JavaScript 1', body: 'Description'},
+    {id: 2, title: 'JavaScript 2', body: 'Description'},
+    {id: 3, title: 'JavaScript 3', body: 'Description'},
+  ])
+
+    const createPost = (newPost) => {
+      setPosts([...posts, newPost])
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <PostForm create={createPost}/>
+        <PostList posts={posts} title={"Посты про JS"}/>
     </div>
   );
 }
